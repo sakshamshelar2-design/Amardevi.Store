dd7b8e50463c39f97088b48d64f11a7daecddebb
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-23 23:05:49 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -570,7 +570,53 @@ export const products: Product[] = [
     description: 'Pure cooking oil - 1 liter',
     inStock: true,
     onSale: true
-  },   
+  },
+  {
+    id: 54,
+    name: 'Chole Masala Everst',
+    price: 145,
+    originalPrice: 165,
+    image: 'https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=400',
+    category: 'Masala',
+    description: 'Pure cooking oil - 1 liter',
+    inStock: true,
+    onSale: true
+  },
+  {
+    id: 55,
+    name: 'Misal Masala',
+    price: 145,
+    originalPrice: 165,
+    image: 'https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=400',
+    category: 'Masala',
+    description: 'Pure cooking oil - 1 liter',
+    inStock: true,
+    onSale: true
+  },
+  {
+    id: 56,
+    name: 'Samber Masala',
+    price: 145,
+    originalPrice: 165,
+    image: 'https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=400',
+    category: 'Masala',
+    description: 'Pure cooking oil - 1 liter',
+    inStock: true,
+    onSale: true
+  },
+  {
+    id: 57,
+    name: 'Sabji Masala',
+    price: 145,
+    originalPrice: 165,
+    image: 'https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=400',
+    category: 'Masala',
+    description: 'Pure cooking oil - 1 liter',
+    inStock: true,
+    onSale: true
+  },
+
+  
   
 ];
 
