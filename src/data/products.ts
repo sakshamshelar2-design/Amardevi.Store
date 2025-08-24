eff1b5bd20ee867b761d885e55f22c3a3d54cf77
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-24 22:12:58 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -490,7 +490,7 @@ export const products: Product[] = [
     name: 'Parle Monaco',
     price: 145,
     originalPrice: 165,
-    image: 'https://www.jiomart.com/images/product/original/490087809/parle-monaco-classic-regular-salted-biscuits-52-2-g-product-images-o490087809-p490087809-0-202302282150.jpg?im=Resize=(420,420)?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://m.media-amazon.com/images/I/71Nh8WJmWNL._SL1500_.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Biscuits',
     description: 'Pure cooking oil - 1 liter',
     inStock: true,
