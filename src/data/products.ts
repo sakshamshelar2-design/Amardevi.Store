8b6a2f231f21888268dfd01bcab21bc78f79b101
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-24 22:09:51 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -512,7 +512,7 @@ export const products: Product[] = [
     name: 'Dark Fantasy',
     price: 145,
     originalPrice: 165,
-    image: 'https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://m.media-amazon.com/images/I/81lRRRqzykL.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Biscuits',
     description: 'Pure cooking oil - 1 liter',
     inStock: true,
