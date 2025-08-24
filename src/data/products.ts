f4f09967c2ccbdeeffbf825d565380ff8a2222f1
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-24 14:13:36 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -402,7 +402,7 @@ export const products: Product[] = [
     name: 'Good day',
     price: 145,
     originalPrice: 165,
-    image: 'https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://www.bbassets.com/media/uploads/p/xl/270729_21-britannia-good-day-cashew-cookies.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Biscuits',
     description: 'Pure cooking oil - 1 liter',
     inStock: true,
