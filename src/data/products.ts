93517bfebb0a330e08fb252846a0779dbb79ed1b
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-25 19:09:59 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1035,6 +1035,36 @@ export const products: Product[] = [
       { id: '2-10kg', weight: '10 kg', price: 470 }
     ]
   },
+   {
+    id: 89,
+    name: 'Tide',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
+   {
+    id: 83,
+    name: 'Vanish',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
   
   
   
