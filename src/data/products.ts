d2bbf1897ffb60ed9c70cf7f157dd4472022d618
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-25 19:00:40 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -943,6 +943,100 @@ export const products: Product[] = [
     inStock: true,
     onSale: true
   },
+
+  // Cleaning 
+   {
+    id: 83,
+    name: 'Vim Bar',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
+   {
+    id: 84,
+    name: 'Sargam Bar',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
+   {
+    id: 85,
+    name: 'Xpert Bar',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
+   {
+    id: 86,
+    name: 'Exo Bar',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
+   {
+    id: 87,
+    name: 'Patanjali Super Bar',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
+   {
+    id: 88,
+    name: '',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
+  
+  
   
   
   
