bd2f00a0ff8c4bdc8a26079bf336e50627df7f7e
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-25 23:14:06 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1686,13 +1686,18 @@ export const products: Product[] = [
   //Dal
     {   
     id: 136,
-    name: 'hi',
+    name: 'Sabudane',
     price: 450,
     image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
-    category: 'Snacks',
+    category: 'Dals',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
     featured: true,
+      variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
    },
   
   
