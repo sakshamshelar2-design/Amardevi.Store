6875b0d1c8382057bd1e443fa30368668d6b232c
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-25 19:24:21 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1170,6 +1170,68 @@ export const products: Product[] = [
       { id: '2-10kg', weight: '10 kg', price: 470 }
     ]
   },
+   {
+    id: 98,
+    name: 'Santoor',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
+   {
+    id: 99,
+    name: 'Moti Sandal',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
+   {
+    id: 100,
+    name: 'Life Boy',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
+   {
+    id: 101,
+    name: 'Dove',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Cleaning',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+     variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+  },
+  
+  
   
   
   
