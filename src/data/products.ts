bf3f016596b3e76c6516912a68b8ad9169ad1d0c
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-25 23:17:20 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1701,11 +1701,26 @@ export const products: Product[] = [
    },
    {   
     id: 136,
-    name: 'Shangdane',
+    name: 'Penuts',
     price: 450,
     image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
     category: 'Dals',
     description: 'Premium Qality penuts',
+    inStock: true,
+    featured: true,
+      variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+   },
+   {   
+    id: 136,
+    name: '',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Dals',
+    description: 'Aromatic coffee beans - 500g',
     inStock: true,
     featured: true,
       variants: [
