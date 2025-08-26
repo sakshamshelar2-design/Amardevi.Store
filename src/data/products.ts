26ee7b825efbd403bc11dc5d8d06e8f5292d3b8e
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-26 07:31:03 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1910,6 +1910,67 @@ export const products: Product[] = [
       { id: '2-10kg', weight: '10 kg', price: 470 }
     ]
    },
+  //kitchen
+    {   
+    id: 151,
+    name: 'Gemini Sunflower',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Kitchen',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+      variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+    },
+      {   
+    id: 153,
+    name: 'Sunny Sunflower',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Kitchen',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+        variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+      },
+      {   
+    id: 154,
+    name: 'Sunny Soyabean',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Kitchen',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+        variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+      },
+    {   
+    id: 153,
+    name: 'Sunny Groundnut Oil',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Kitchen',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+        variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+    },     
   
   
   
