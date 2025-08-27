a7f0f294872f781b4be5951c0bc0285d536d6cdc
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 10:59:25 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -952,7 +952,7 @@ export const products: Product[] = [
     id: 83,
     name: 'Vim Bar',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://m.media-amazon.com/images/I/61szrCRWOEL._SX569_.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Cleaning',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
