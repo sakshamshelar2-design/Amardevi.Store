4826efe844a3ab881ce4f893b220ee39c28aeb5e
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 14:07:55 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1680,7 +1680,7 @@ export const products: Product[] = [
     id: 135,
     name: 'Soya sticks',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://www.haldirams.com/media/catalog/product/cache/71134970afb779eb7860339989626b7e/s/o/soya_sticks200_1.png?auto=compress&cs=tinysrgb&w=400',
     category: 'Snacks',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
