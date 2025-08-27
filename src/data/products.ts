db440efd4b2b7fdaad8f87b9a15c53234e02d67f
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 12:20:35 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1147,7 +1147,7 @@ export const products: Product[] = [
     id: 96,
     name: 'Ghadi Detergent',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://5.imimg.com/data5/JX/CT/EV/SELLER-103215204/ghadi-detergent-powder-1kg-1000x1000.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Cleaning',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
