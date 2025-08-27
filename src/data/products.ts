75d628b86e6c6267ea6945edc698727847d71524
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 09:33:56 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -863,7 +863,7 @@ export const products: Product[] = [
     name: 'Paneer Butter Masala  ',
     price: 145,
     originalPrice: 165,
-    image: 'https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://m.media-amazon.com/images/I/81cA6i92hIL._UF1000,1000_QL80_.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Masala',
     description: 'Pure cooking oil - 1 liter',
     inStock: true,
