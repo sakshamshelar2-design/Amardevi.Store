3174b71c6a2669cca8e2fc4a568a4a322d78f417
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 09:38:17 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -885,7 +885,7 @@ export const products: Product[] = [
     name: 'Mutter Paneer Masala ',
     price: 145,
     originalPrice: 165,
-    image: 'https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://suhana.com/cdn/shop/files/SM-MP-1-1.jpg?v=1707833539?auto=compress&cs=tinysrgb&w=400',
     category: 'Masala',
     description: 'Pure cooking oil - 1 liter',
     inStock: true,
