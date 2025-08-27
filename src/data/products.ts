b8b1e15cad600b51c9fd8d7a400a7c2f1064a13a
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 09:22:53 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -687,7 +687,7 @@ export const products: Product[] = [
     name: 'Chicken Masala Suhana   ',
     price: 145,
     originalPrice: 165,
-    image: 'https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://suhana.com/cdn/shop/files/SUH-CM-200g-pouch-preview.png?v=1707833846?auto=compress&cs=tinysrgb&w=400',
     category: 'Masala',
     description: 'Pure cooking oil - 1 liter',
     inStock: true,
