a462eda48d10c0d88266589f43f815ebd1d76330
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 11:00:59 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -982,7 +982,7 @@ export const products: Product[] = [
     id: 85,
     name: 'Xpert Bar',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://m.media-amazon.com/images/I/41BiNkUT7nL._UF1000,1000_QL80_.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Cleaning',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
