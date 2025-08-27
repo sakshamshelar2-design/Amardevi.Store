18a6662eec4a58b0038c674d306ae14cec4e3f94
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 14:25:13 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1918,7 +1918,7 @@ export const products: Product[] = [
     id: 151,
     name: 'Gemini Sunflower',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://rukminim2.flixcart.com/image/704/844/xif0q/edible-oil/v/6/d/-original-imahcarehgzpfwtg.jpeg?q=90&crop=false?auto=compress&cs=tinysrgb&w=400',
     category: 'Kitchen',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
