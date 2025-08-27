e524cb54f5ebc3519b3e2b885cddf98a80765ed7
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 09:07:52 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -577,7 +577,7 @@ export const products: Product[] = [
     name: 'Halad Powder',
     price: 145,
     originalPrice: 165,
-    image: 'https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://suhana.com/cdn/shop/files/AMB-TUR-100g-preview.png?v=1707833798?auto=compress&cs=tinysrgb&w=400',
     category: 'Masala',
     description: 'Pure cooking oil - 1 liter',
     inStock: true,
