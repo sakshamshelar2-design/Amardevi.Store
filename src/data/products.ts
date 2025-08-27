8bf31f063d587f4eafb7022ae6219239ea40c216
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 11:08:01 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1057,8 +1057,7 @@ export const products: Product[] = [
     id: 90,
     name: 'Vanish',
     price: 450,
-    image: 'https://www.vanish.co.in/static/feff9737370a5ab7a619d3d230801b5d/83d1b/vanish-oxi-action-liquid.png
-  ?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://www.vanish.co.in/static/feff9737370a5ab7a619d3d230801b5d/83d1b/vanish-oxi-action-liquid.png?auto=compress&cs=tinysrgb&w=400',
     category: 'Cleaning',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
