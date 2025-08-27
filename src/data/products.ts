441a387bf873b1f618c41b22fd32cbd1346a9f26
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 13:16:31 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1600,7 +1600,7 @@ export const products: Product[] = [
     id: 127,
     name: 'VijayLaxmi Red Farsan',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://m.media-amazon.com/images/I/41vsNGajE0L.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Snacks',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
