f6c9dae1bc25a4cf49f65c928c34018a97f944ac
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 13:02:41 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1447,7 +1447,7 @@ export const products: Product[] = [
     id: 116,
     name: 'Pepsodent',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://gandhifood.com/cdn/shop/files/Pepsodent-Toothpaste-Whitening-Cavity-Protection-150-g_dcd4bf3f-7b60-4138-9c85-8c3cc6570f24.e8da060b0c33c81b866122ceb1841627_1024x.jpg?v=1705175698?auto=compress&cs=tinysrgb&w=400',
     category: 'Cleaning',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
