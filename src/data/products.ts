33e355f773333f1fede848313deb3caaf91cd6e9
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 13:00:30 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1387,7 +1387,7 @@ export const products: Product[] = [
     id: 112,
     name: 'Colgate Maxfresh Blue',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://m.media-amazon.com/images/I/61aPreWIR6L._UF1000,1000_QL80_.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Cleaning',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
