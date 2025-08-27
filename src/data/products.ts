f56504987245f4534bb3851c526dc39806cc6b16
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 13:06:54 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1462,7 +1462,7 @@ export const products: Product[] = [
     id: 117,
     name: 'Patanjali Dant kanti',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://m.media-amazon.com/images/I/71gmZKTUXnL._UF1000,1000_QL80_.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Cleaning',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
