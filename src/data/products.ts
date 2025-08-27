f934ad4f060030ab806ae01f8b1940d633b50bf2
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 13:02:08 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1432,7 +1432,7 @@ export const products: Product[] = [
     id: 115,
     name: 'Dabur Meswak',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://m.media-amazon.com/images/I/71JdxSoiNnL.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Cleaning',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
