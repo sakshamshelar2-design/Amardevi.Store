13be810be9e73f27486c0e9b9b1c8053d583f1c1
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 09:25:16 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -753,7 +753,7 @@ export const products: Product[] = [
     name: 'Pani puri Masala ',
     price: 145,
     originalPrice: 165,
-    image: 'https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://m.media-amazon.com/images/I/81dRPmHKxyL.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Masala',
     description: 'Pure cooking oil - 1 liter',
     inStock: true,
