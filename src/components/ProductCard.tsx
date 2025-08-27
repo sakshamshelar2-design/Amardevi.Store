f371d1e53dc52545a904d73f2f322637464a91e0
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 09:49:36 +0500

    Updated ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -56,7 +56,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
         <img
           src={product.image}
           alt={product.name}
-          className="w-full h-50 object-cover group-hover:scale-105 transition-transform duration-300"
+          className="w-full h-20 object-cover group-hover:scale-105 transition-transform duration-300"
         />
         {product.onSale && (
           <div className="absolute top-2 left-2">
