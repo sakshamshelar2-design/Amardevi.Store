09a192da91f90059dd6c6ffc75dafa8545259a9b
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 14:08:31 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1691,7 +1691,7 @@ export const products: Product[] = [
     id: 136,
     name: 'Sabudane',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://nuttyyogi.com/cdn/shop/products/CrunchyChatpataCashews_5.png?v=1680770153?auto=compress&cs=tinysrgb&w=400',
     category: 'Dals',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
