aaa7905e86cb9f9d46d38192daca5e33286bea83
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 11:02:45 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1012,7 +1012,7 @@ export const products: Product[] = [
     id: 87,
     name: 'Patanjali Super Bar',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://www.bbassets.com/media/uploads/p/l/40016330_8-patanjali-super-dishwash-bar.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Cleaning',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
