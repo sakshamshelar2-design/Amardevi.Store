a053b6c7c4cdcbd5c59c1976ba93021d02c8d993
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-27 14:12:27 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1766,7 +1766,7 @@ export const products: Product[] = [
     id: 141,
     name: 'Dhane',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwYtF6vDVlmHelMtJLKYcuV1qBvFKy3kLWfQ&s?auto=compress&cs=tinysrgb&w=400',
     category: 'Dals',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
