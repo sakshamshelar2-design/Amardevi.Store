be4ddf382ffefeb17c9f75009f70d8240288ba75
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-30 23:36:38 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -2099,7 +2099,21 @@ export const products: Product[] = [
       { id: '2-10kg', weight: '10 kg', price: 470 }
     ]
     }, 
-  
+   {   
+    id: 163,
+    name: 'Wagh Bakri',
+    price: 450,
+    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    category: 'Tea&Coffee',
+    description: 'Aromatic coffee beans - 500g',
+    inStock: true,
+    featured: true,
+       variants: [
+      { id: '2-1kg', weight: '1 kg', price: 52 },
+      { id: '2-5kg', weight: '5 kg', price: 245 },
+      { id: '2-10kg', weight: '10 kg', price: 470 }
+    ]
+    }, 
   
   
   
