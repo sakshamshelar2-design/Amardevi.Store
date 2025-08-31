1f26228dbdaf9c40f2681e1528a1f8eb8651f680
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-31 10:09:52 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -2013,7 +2013,7 @@ export const products: Product[] = [
     id: 159,
     name: 'Samrat Maida ',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://m.media-amazon.com/images/I/71ti2SgDXOL.jpg?auto=compress&cs=tinysrgb&w=400',
     category: 'Kitchen',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
