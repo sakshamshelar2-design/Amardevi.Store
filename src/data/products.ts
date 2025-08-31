4d411d248c9ff0776a071626b71f7565bedf7117
sakshamshelar2-design <sakshamshelar2@gmail.com>
2025-08-31 10:17:18 +0500

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -2133,7 +2133,7 @@ export const products: Product[] = [
     id: 167,
     name: 'Vikram Dust',
     price: 450,
-    image: 'https://sfbaycoffee.com/cdn/shop/articles/SFB_LFS_BeansVsGrounds_blog.jpg?v=1686342662?auto=compress&cs=tinysrgb&w=400',
+    image: 'https://www.vikramtea.com/cdn/shop/files/26_Kadakdustfront.jpg?v=1744893978?auto=compress&cs=tinysrgb&w=400',
     category: 'Tea&Coffee',
     description: 'Aromatic coffee beans - 500g',
     inStock: true,
